c182e6544b393a5064841a7fc146c87eac27d405
daxapps <daxapps777@gmail.com>
2017-04-08 17:05:37 -0500

    Enable user to add(post) campground
    - add bootstrap

--- v1/app.js
+++ v1/app.js
@@ -1,19 +1,22 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+    var campgrounds = [
+            {name: "Salmon Creek", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
+            {name: "Granite Hill", image: "https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
+            {name: "Mountain Goat's Rest", image: "https://farm5.staticflickr.com/4137/4812576807_8ba9255f38.jpg"}
+            ];
+            
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-            {name: "Salmon Creek", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
-            {name: "Granite Hill", image: "https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
-            {name: "Mountain Goat's Rest", image: "https://farm5.staticflickr.com/4137/4812576807_8ba9255f38.jpg"}
-            ]
-            
             res.render("campgrounds", {campgrounds:campgrounds});
 });
 
@@ -21,7 +24,19 @@ app.get("/campgrounds", function(req, res){
 
 
 
+app.post("/campgrounds", function(req, res){
+    //get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    //redirect back to campgrounds page
+    res.redirect("/campgrounds");
+});
 
+app.get("/campgrounds/new", function(req, res){
+    res.render("new.ejs");
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The Yelp Camp Server Has Started!!");
