049acc239654644ef52f4ec149f07841ae9c9ad7
daxapps <daxapps777@gmail.com>
2017-04-08 17:57:04 -0500

    Improve UI

--- v1/app.js
+++ v1/app.js
@@ -8,6 +8,9 @@ app.set("view engine", "ejs");
     var campgrounds = [
             {name: "Salmon Creek", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
             {name: "Granite Hill", image: "https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
+            {name: "Mountain Goat's Rest", image: "https://farm5.staticflickr.com/4137/4812576807_8ba9255f38.jpg"},
+            {name: "Salmon Creek", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
+            {name: "Granite Hill", image: "https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
             {name: "Mountain Goat's Rest", image: "https://farm5.staticflickr.com/4137/4812576807_8ba9255f38.jpg"}
             ];
             
